3b16f4d57351d5af7a950cf1d47efe2a31a036f2
Glenn Matthew C Senoc <glennworks69@gmail.com>
2025-09-14 00:11:30 +0800

    A simple profix expression evaluation java code.
    Code utilizes postfix notation for less ambiguity of infix expressions and makes it easy for coding implementations.

---
+++ SimpleEvaluator/src/ExpressionConverter.java
@@ -0,0 +1,57 @@
+import java.util.List;
+import java.util.Stack;
+
+public class ExpressionConverter {
+
+    public double evaluatePostfix(String postfix) {
+        Stack<Double> stack = new Stack<>();
+
+        //loops each component by splitting
+        for (String component : postfix.split(" ")) {
+            if (!isOperator(component)) {
+                stack.push(Double.parseDouble(component));
+            } else {
+                double num2 = stack.pop();
+                double num1 = stack.pop();
+                switch (component) {
+                    //push the result
+                    case "*" -> stack.push(num1 * num2);
+                    case "/" -> stack.push(num1 / num2);
+                    case "+" -> stack.push(num1 + num2);
+                    case "-" -> stack.push(num1 - num2);
+                    default -> throw new IllegalArgumentException("Invalid operator: " + component);
+                }
+            }
+            stack.peek();
+        }
+        return stack.pop();
+    }
+
+    //determines operator
+    public boolean isOperator(String operator) {
+        return List.of("*", "/", "+", "-").contains(operator);
+    }
+
+
+
+
+
+
+    /*
+    //for sorting precedences
+    public static boolean hasLowerPrecedence(String op1, String op2) {
+        return precedence(op1) < precedence(op2);
+    }
+    public static int precedence(String operator) {
+        return switch (operator) {
+            case "+", "-" -> 1;
+            case "*", "/" -> 2;
+            default -> 3;
+        };
+    }
+    */
+}
+
+
+
+
