57b37cedaeeb1d1e4ec300c96b89b10d953b2aaf
Glenn Matthew C Senoc <glennworks69@gmail.com>
2025-09-17 22:40:41 +0800

    Add files via upload
    A console-based Java application that simulates a port authority's management system for containers and ships. This project demonstrates the fundamental concepts of **stacks** and **queues** using a `LinkedList` implementation.

---
+++ PortContainerManagement/src/Ship.java
@@ -0,0 +1,14 @@
+public class Ship {
+    private String name;
+    private String captain;
+
+    public Ship(String name, String captain) {
+        this.name = name;
+        this.captain = captain;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s | %-12s", name, captain);
+    }
+}
