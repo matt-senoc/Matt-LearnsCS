c98d67f74d77683cef85476906cb91d588ea9a86
Glenn Matthew C Senoc <glennworks69@gmail.com>
2025-09-29 14:57:52 +0800

    Update Exxam.java

--- src/Exxam.java
+++ src/Exxam.java
@@ -1,9 +1,72 @@
+package nicker;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
 
-public class Exxam {
+public class exam {
 	
 	public static void main(String[] args) {
-		System.out.println("1");
+	
+		Scanner sc = new Scanner(System.in);
+		int decision;
+	
+		do {
+			System.out.println("Enter option: "
+					+ "\n1. Queue a patient."
+					+ "\n2. Display patient queue."
+					+ "\n3. Treat a patient."
+					+ "\n4. Exit.");
+			decision = sc.nextInt();
+		switch (decision) {
+				case 1: break;
+				case 2: break;
+				case 3: break;
+				case 4: break;
+				
+				
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + decision);
+			}
+		
+		} while (decision > -1);
+		
+		
 		
 	}
+	
+	class queueMeth(){
 
+		System.out.println("Enter patient name:");
+		String Pname = sc.next();
+	
+		System.out.println("Enter patient name:");
+		int prio = sc.nextInt();
+		
+		System.out.println("Enter patient name:");
+		String med_condition= sc.next();
+		
+		System.out.println("Enter patient name:");
+		String time = sc.next();
+		
+		ERQueue er = new ERQueue();
+		er.arrive(Pname, prio, med_condition, time);
+	}
 }
+/*
+
+fields: 
+	name, medical condition, priority level, arrival time
+	same priority level are determined by arrival time
+	
+IMPLEMENTED USING PRIORITY QUEUE DS
+
+classes:
+	string name
+	int prio
+	string med condition
+	string arrivalTime
+	
+void arrive(String name, int priority, String condition, String time)	
+void treatNext() → removes and displays the next patient to be treated
+void displayQueue() → prints the current queue in the format below	
+*/
